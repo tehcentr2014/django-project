7a2e1c4ccc46445d77d544d8c7b9151f4ddddde4
Mary na <tehcentr2014@gmail.com>
2024-05-16 15:00:54 +0000

    Heroku settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h7kvl=snqvuru%c^g23^pcujar$!$1y!!h^9-lq-05t-49ugyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-tehcentr201-djangoproje-qjszs4j5zcr.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['8000-tehcentr201-djangoproje-qjszs4j5zcr.ws-eu111.gitpod.io','.herokuapp.com']
 
 
 # Application definition
